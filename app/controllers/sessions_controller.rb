dc9193d52667bc77dd34b6ab8f99357e2836c3d1
Greg <gjtrowbridge@gmail.com>
2013-12-21 23:26:29 -0500

    Finish user signin/signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,19 @@ class SessionsController < ApplicationController
 
   end
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Sign in the user and redirect to user page
+      sign_in user
+      redirect_to user
+    else
+      #Create error message and re-render signin form
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
   def destroy
-
+    sign_out
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "New account created successfully!"
-      redirect_to root_path
+      redirect_to @user
     else
       render 'new'
     end
