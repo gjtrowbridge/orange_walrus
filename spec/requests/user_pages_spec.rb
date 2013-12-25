441bf78e4b29925c807c070cb04ed7625b278e13
Greg <gjtrowbridge@gmail.com>
2013-12-25 02:28:21 -0500

    Allow users to update profiles

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,13 +13,10 @@ describe "UserPages" do
 
   describe "signup page" do
     before { visit signup_path }
+    let(:submit) { "Create my account" }
+
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
-  end
-
-  describe "signup page" do
-    before { visit signup_path }
-    let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       it "should not create a user" do
@@ -49,4 +46,37 @@ describe "UserPages" do
     end
   end
 
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      valid_signin user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit User") }
+    end
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Display name",     with:new_name
+        fill_in "Email",            with:new_email
+        fill_in "Password",         with:user.password
+        fill_in "Confirm Password", with:user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out') }
+      specify { expect(user.reload.display_name).to eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+  end
 end
